4cbd1ab117d959cb948e6f98b7c012842b10f4dc
Tellay <tellayprivadobuilds@gmail.com>
2025-11-16 21:50:17 +0000

    setup prettier/eslint, and shadcn/ui

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex h-full min-h-screen flex-col items-center justify-center gap-2 p-4">
+      <h1>Hello World from Next.js</h1>
+
+      <Button>Example Button Without Action</Button>
+    </main>
+  );
+}
